89fb057d38aa701dcc7493db5b5dc97e22072a0a
Kauã <kauatiezzi02@hotmail.com>
2026-03-03 20:16:25 -0300

    V2

---
+++ src/main/java/org/example/ExPersonalizada.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class ExPersonalizada {
+
+    static void sacar(double saldo, double valor) throws
+    SaldoInsuficienteException {
+        if (valor > saldo) {
+            throw new SaldoInsuficienteException("Saldo Insuficiente! Saldo: " + saldo);
+        }
+    }
+
+
+    static void main(String[] args) {
+
+        try {
+            sacar(100,500);
+        } catch (SaldoInsuficienteException e ){
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+}
